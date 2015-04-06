58edac06fcda04553e9546cf84fa00580f258f05
rmcdaniel <richard.mcdaniel@uah.edu>
2015-04-06 18:00:28 +0000

    first commit

---
+++ mysh.cpp
@@ -0,0 +1,315 @@
+// Students   : Zachary Wilson, Richard McDaniel
+// Assignment : CS390 Spring 2015 Project
+// Date       : 3/28/15
+
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
+#include <unistd.h>
+
+using namespace std;
+
+bool checkSize(string command);
+void error();
+
+/* Global Variables */
+
+string command = "";
+string prompt = "mysh> ";
+string version  = "mysh version 1.0 Copyright 2014 by Zachary Wilson, Richard McDaniel";
+string filename = "";
+bool redirect = false;
+bool append = false;
+
+//COMMANDS AVAILABLE
+//chpr = Change Prompt : allow user to change the prompt output
+//ver  = Show Shell Version
+//exit = Exit the program
+
+//I am also checking the size of the entered command and holding it under 255 chars
+
+
+/*
+This function outputs a line of text to a file or standard output.
+@param string line - a line of text to output
+@returns nothing
+*/
+void output(string line)
+{
+    if (filename == "")
+    {
+        cout << line << endl;
+    }
+    else
+    {
+        ofstream fout;
+        if (append)
+        {
+            fout.open(filename.c_str(), std::ofstream::app);
+        }
+        else
+        {
+            fout.open(filename.c_str());
+        }
+        fout << line << endl;
+        fout.close();
+    }
+}
+
+/*
+This function executes a command and outputs the result to a file or STDOUT.
+@returns nothing
+*/
+int execute(string command)
+{
+    int pipes[3] = {0};
+    
+    char* argv[256];
+    // argv[0] = "ls";
+    // argv[1] = NULL;
+    
+    int pid;
+    
+    int in[2];
+    int out[2];
+    int err[2];
+    
+    
+    pipe(in);
+    pipe(out);
+    pipe(err);
+    
+    pid = fork();
+    
+    if (pid > 0)
+    {
+        close(in[0]);
+        close(out[1]);
+        close(err[1]);
+        
+        pipes[0] = in[1];
+        pipes[1] = out[0];
+        pipes[2] = err[0];
+    }
+    else
+    {
+        close(in[1]);
+        close(out[0]);
+        close(err[0]);
+        
+        if (pipes[1] != 0)
+        {
+            in[0] = pipes[1];
+        }
+        
+        dup2(in[0], STDIN_FILENO);
+        dup2(out[1], STDOUT_FILENO);
+        dup2(err[1], STDERR_FILENO);
+        
+        execvp(argv[0], argv);
+        exit(1);
+    }
+    
+    string result = "";
+    char nChar;
+    
+    while (read(pipes[1], &nChar, 1) == 1) {
+        result += nChar;
+    }
+    
+    output(result);
+    
+    close(pipes[0]);
+    close(pipes[1]);
+    close(pipes[2]);
+    
+    return pid;
+}
+
+void parse(string line)
+{
+    bool escaping = false;
+    bool quoting = false;
+    bool redirecting = false;
+    bool appending = false;
+    
+    vector<string> argv;
+    string word = "";
+    
+    filename = "";
+    redirect = false;
+    append = false;
+    
+    for (string::size_type i = 0; i < line.size(); ++i) {
+        switch (line[i])
+        {
+            case ' ':
+            if (quoting)
+            {
+                word += line[i];
+            }
+            else
+            {
+                if ((word != "") && (word != " ")) {
+                    if (redirecting) {
+                        argv.push_back(">");
+                        redirecting = false;
+                    }
+                    else if (appending) {
+                        argv.push_back(">>");
+                        appending = false;
+                    }
+                    argv.push_back(word);
+                }
+                word = "";
+            }
+            break;
+            
+            case '>':
+            if (escaping)
+            {
+                escaping = false;
+                word += line[i];
+            }
+            else if (redirecting)
+            {
+                redirecting = false;
+                appending = true;
+                word = "";
+            }
+            else
+            {
+                redirecting = true;
+                if ((word != "") && (word != " ")) {
+                    argv.push_back(word);
+                }
+                word = "";
+            }
+            break;
+            
+            case '\\':
+            if (escaping)
+            {
+                escaping = false;
+                word += line[i];
+            }
+            else
+            {
+                escaping = true;
+            }
+            break;
+            
+            case '"':
+            if (escaping)
+            {
+                escaping = false;
+                word += line[i];
+            }
+            else
+            {
+                quoting = !quoting;
+            }
+            break;
+            
+            default:
+            word += line[i];
+        }
+    }
+    
+    if (word != "")
+    {
+        if (redirecting) {
+            argv.push_back(">");
+            redirecting = false;
+        }
+        else if (appending) {
+            argv.push_back(">>");
+            appending = false;
+        }
+        argv.push_back(word);
+    }
+    
+    if (argv.size() >= 3)
+    {
+        redirect = argv.end()[-2] == ">";
+        append = argv.end()[-2] == ">>";
+    }
+    
+    if ((argv.size() > 0) && (redirect || append)) {
+        filename = argv.back();
+    }
+    
+    if (argv[0] == "chpr")
+    {
+        
+        cout << "Please enter new prompt: " ;
+        getline(cin, prompt);
+        
+        cout << "Prompt changed to : [" << prompt << "] sucessfully" << endl;
+    }
+    else if (argv[0] == "ver")
+    {
+        output("Shell Version : " + version);
+    }
+    else if (argv[0] == "cd")
+    {
+        output(getenv("HOME"));
+    }
+    else
+    {
+        if(argv[0] != "exit")
+        {
+            error();
+        }
+    }
+}
+
+
+int main()
+{
+    cout << "Welcome to the mysh shell !!! " << endl;
+    
+    command = "ver";
+    
+    while (command != "exit")
+    {
+        cout << prompt;
+        getline(cin, command);
+        bool in_bounds = checkSize(command);
+        
+        if (in_bounds)
+        {
+            parse(command);
+        }
+        else
+        {
+            error();
+        }
+    }
+    
+    output("Exiting Shell...");
+    
+    return 0;
+}
+
+bool checkSize(string command)
+{
+    int std_size = 255;
+    
+    if (command.size() <= std_size)
+    {
+        return true;
+    }
+    else
+    {
+        error();
+        return false;
+    }
+}
+
+void error()
+{
+    output("mysh: Error detected");
+}
